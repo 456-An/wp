5db3710e6a6148aa399345f7f2552e847966901f
456-An <161337878+456-An@users.noreply.github.com>
2024-06-01 10:56:39 +0800

    Create main.js

---
+++ week10/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#a1").click(function(){
+        $("#item1").text("人興財旺富貴長");
+        $("#item2").text("龍行大運");
+        $("#item4").text("龍騰虎躍家興旺");
+    });
+    $("#a2").click(function(){
+        $("#item1").text("龍騰盛世領春來");
+        $("#item2").text("前程似錦");
+        $("#item4").text("兔搭神舟登月去");
+    });
+    $("#a3").click(function(){
+        $("#item1").text("飛天彩鳳舞新春");
+        $("#item2").text("一帆風順");
+        $("#item4").text("出海金龍吟盛世");
+    });
+});
